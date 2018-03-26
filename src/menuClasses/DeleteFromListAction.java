e1efba13ac72ef967a11d0d0edf1e3f81613ac95
ICOM STUDENTS <4035b13@icaco.ece.uprm.edu>
2018-03-26 13:17:46 -0400

    updated menu

--- src/menuClasses/DeleteFromListAction.java
+++ src/menuClasses/DeleteFromListAction.java
@@ -1,11 +1,19 @@
 package menuClasses;
 
+import dataManager.DMComponent;
+import ioManagementClasses.IOComponent;
+
 public class DeleteFromListAction implements Action {
 
 	@Override
 	public void execute(Object args) {
 		// TODO Auto-generated method stub
-
+		DMComponent dm = (DMComponent) args; 
+		IOComponent io = IOComponent.getComponent(); 
+		io.output("\nDeleting an list of Integer values to the system:\n"); 
+		String listName = io.getInput("\nEnter name of list to delete: ");
+		
+		//dm.removeElementFromList(listName,);
 	}
 
 }
